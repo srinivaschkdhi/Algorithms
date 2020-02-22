334d9ec4ad960550febaefcef1be31a589c858c1
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2020-02-22 23:53:39 +0530

    Modified Min in rotated sorted array i.e pivot in sorted rotated array

--- src/main/java/searching/MinInRotatedSortedArray.java
+++ src/main/java/searching/MinInRotatedSortedArray.java
@@ -1,3 +1,4 @@
+package searching;
 
 public class MinInRotatedSortedArray {
     public int findMin(int[] A) {
@@ -24,6 +25,36 @@ public class MinInRotatedSortedArray {
         }
 
         return A[low];
+    }
+
+    public int findPivot(int[] array) {
+        if (array == null || array.length == 0)
+            return 0;
+
+        // Case when array is not rotated. Then first index is the pivot
+        if (array.length == 1 || array[0] < array[array.length - 1]) {
+            return 0;
+        }
+
+        int low = 0;
+        int high = array.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (mid < array.length - 1 && array[mid] > array[mid + 1])
+                return array[mid + 1];
+            else if (array[low] <= array[mid]) {
+                // If array[start] <= array[mid],
+                // it means from start to mid, all elements are in sorted order,
+                // so pivot will be in second half
+                low = mid + 1;
+            } else
+                // else pivot lies in first half, so we find the pivot in first half
+                high = mid - 1;
+        }
+
+        return 0;
     }
 
 }
